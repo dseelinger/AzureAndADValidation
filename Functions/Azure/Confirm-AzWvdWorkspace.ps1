14a0592f48777df1f0e41891769010936ad0859c
Doug Seelinger <doseelin@microsoft.com>
2024-08-19 11:02:25 -0400

    Completed Confirm-AzWvdWorkspace

---
+++ Functions/Azure/Confirm-AzWvdWorkspace.ps1
@@ -0,0 +1,48 @@
+function Confirm-AzWvdWorkspace {
+    <#
+    .SYNOPSIS
+    Tests for the existence of a Windows Virtual Desktop Workspace in Azure.
+
+    .DESCRIPTION
+    The Confirm-AzWvdWorkspace function takes the name of a Workspace as input and returns $true if it is found,
+    otherwise returns $false.
+
+    .PARAMETER WorkspaceName
+    The name of the Windows Virtual Desktop Workspace to look for.
+
+    .PARAMETER ResourceGroupName
+    The name of the Resource Group that the Workspace is supposed to be in.
+
+    .EXAMPLE
+    Confirm-AzWvdWorkspace -WorkspaceName "MyWvdWorkspace" -ResourceGroupName "MyResourceGroup"
+    Returns $true or $false
+
+    .NOTES
+    Author: Doug Seelinger
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$WorkspaceName,
+        [Parameter(Mandatory=$true)]
+        [string]$ResourceGroupName
+    )
+    begin {
+        Import-Module Az.Accounts
+        Import-Module Az.DesktopVirtualization
+        if (-not (Get-AzContext)) {
+            Connect-AzAccount
+        }
+    }
+    process {
+        try {
+            $workspace = Get-AzWvdWorkspace -Name $WorkspaceName -ResourceGroupName $ResourceGroupName `
+                -ErrorAction Stop
+            return $null -ne $workspace
+        } catch {
+            return $false
+        }
+    }
+    end {
+    }
+}
---
+++ Functions/Azure/Confirm-AzWvdWorkspace.tests.ps1
@@ -0,0 +1,73 @@
+BeforeAll {
+    . $PSScriptRoot\Confirm-AzWvdWorkspace.ps1
+
+    $rgName = 'rg-integration-tests'
+    $testWorkspaceName = 'testWorkspace'
+    $fakeWorkspaceName = 'notARealWorkspace'
+}
+
+Describe 'Confirm-AzWvdWorkspace Integration Tests' -Tag 'Integration', 'Azure' {
+    BeforeAll {
+        # Arrange - Create a Resource Group and WVD Workspace to test against
+        $location = 'usgovvirginia'
+        New-AzResourceGroup -Name $rgName -Location $location | Out-Null
+
+        # Create a WVD Workspace
+        New-AzWvdWorkspace -ResourceGroupName $rgName -Name $testWorkspaceName -Location $location | Out-Null
+    }
+    
+    Context 'When the Workspace exists' {
+        It 'returns $true' {
+            # Act
+            $result = Confirm-AzWvdWorkspace -WorkspaceName $testWorkspaceName -ResourceGroupName $rgName
+
+            # Assert
+            $result | Should -BeTrue
+        }
+    }
+
+    Context 'When the Workspace does not exist' {
+        It 'returns $false' {
+            # Act
+            $result = Confirm-AzWvdWorkspace -WorkspaceName $fakeWorkspaceName -ResourceGroupName $rgName
+
+            # Assert
+            $result | Should -BeFalse
+        }
+    }
+
+    AfterAll {
+        Remove-AzResourceGroup -Name $rgName -Force -AsJob
+    }
+}
+
+Describe 'Confirm-AzWvdWorkspace Unit Tests' -Tag 'Unit', 'Azure' {
+    BeforeAll {
+        function Get-AzContext { return $true }
+    }
+    Context 'When the Workspace exists' {
+        It 'returns $true' {
+            # Arrange
+            function Get-AzWvdWorkspace { return [PSCustomObject]@{Name = "testWorkspace"; ResourceGroupName = "rg-integration-tests" } }
+
+            # Act
+            $result = Confirm-AzWvdWorkspace -WorkspaceName $testWorkspaceName -ResourceGroupName $rgName
+
+            # Assert
+            $result | Should -BeTrue
+        }
+    }
+
+    Context 'When the Workspace does not exist' {
+        It 'returns $false' {
+            # Arrange
+            function Get-AzWvdWorkspace { return $null }
+
+            # Act
+            $result = Confirm-AzWvdWorkspace -WorkspaceName $fakeWorkspaceName -ResourceGroupName $rgName
+
+            # Assert
+            $result | Should -BeFalse
+        }
+    }
+}
