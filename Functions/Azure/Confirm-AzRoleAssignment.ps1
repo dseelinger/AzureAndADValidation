548f1e0fdd042c82c26b267799755723bc9bad21
Doug Seelinger <doseelin@microsoft.com>
2024-08-22 17:05:46 -0400

    Completed Confirm-AzRoleAssignment

---
+++ Functions/Azure/Confirm-AzRoleAssignment.ps1
@@ -0,0 +1,67 @@
+function Confirm-AzRoleAssignment {
+    <#
+    .SYNOPSIS
+    Tests for the existence of an Azure Role Assignment and (optionally) its specific configuration in Azure.
+
+    .DESCRIPTION
+    The Confirm-AzRoleAssignment function takes several parameters and returns $true if it is found and matches the specified 
+    configuration, otherwise it returns $false.
+
+    .PARAMETER RoleAssignmentName
+    The name of the Role Assignment to look for. This parameter is required.
+
+    .PARAMETER ResourceGroupName
+    The name of the Resource Group that the Role Assignment is supposed to be in. This parameter is required.
+
+    .PARAMETER PrincipalDisplayName
+    The Display Name for the principal (user, group, or service principal) assigned to the role. This parameter is required.
+
+    .PARAMETER RoleDefinitionName
+    The name of the role definition (e.g., "Contributor", "Reader"). This parameter is optional.
+
+    .PARAMETER Scope
+    The scope of the role assignment (e.g., subscription, resource group, or resource). This parameter is optional.
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$RoleAssignmentName,
+
+        [Parameter(Mandatory = $true)]
+        [string]$ResourceGroupName,
+
+        [Parameter(Mandatory = $true)]
+        [string]$PrincipalDisplayName,
+
+        [Parameter(Mandatory = $false)]
+        [string]$RoleDefinitionName,
+
+        [Parameter(Mandatory = $false)]
+        [string]$Scope
+    )
+    begin {
+        Import-Module Az.Accounts
+        Import-Module Az.Resources
+        if (-not (Get-AzContext)) {
+            Connect-AzAccount
+        }
+    }
+    process {
+        # Get the role assignments in the specified resource group
+        $roleAssignments = Get-AzRoleAssignment -ResourceGroupName $ResourceGroupName
+
+        # Filter the role assignments based on the provided parameters
+        $filteredAssignments = $roleAssignments | Where-Object {
+            $_.RoleAssignmentName -eq $RoleAssignmentName -and
+            $_.DisplayName -eq $PrincipalDisplayName -and
+            ([string]::IsNullOrEmpty($RoleDefinitionName) -or $_.RoleDefinitionName -eq $RoleDefinitionName) -and
+            ([string]::IsNullOrEmpty($Scope) -or $_.Scope -eq $Scope)
+        }
+
+        # Return $true if the role assignment is found, otherwise return $false
+        return $filteredAssignments.Count -gt 0
+    }
+    end {
+    }
+}
---
+++ Functions/Azure/Confirm-AzRoleAssignment.tests.ps1
@@ -0,0 +1,160 @@
+BeforeAll {
+    . $PSScriptRoot\Confirm-AzRoleAssignment.ps1
+
+    # Arrange - Set up variables
+    $location = 'usgovvirginia'
+    $resourceGroupName = "rg-integration-tests"
+    $principalDisplayName = "sp-integration-tests"
+    $roleDefinitionName = "Contributor"
+    $context = Get-AzContext
+    $subscriptionId = $context.Subscription.Id
+    $scope = "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName"
+}
+
+Describe 'Confirm-AzRoleAssignment Integration Tests' -Tag 'Integration', 'Azure' {
+    BeforeAll {
+        # Arrange - Create a service principal and assign it the Contributor role
+        $sp = New-AzADServicePrincipal -DisplayName $principalDisplayName
+        $principalId = $sp.Id
+        New-AzResourceGroup -Name $resourceGroupName -Location $location
+        $roleAssignment = New-AzRoleAssignment -ObjectId $principalId -RoleDefinitionName $roleDefinitionName -Scope $scope
+        $roleAssignmentName = $roleAssignment.RoleAssignmentName
+    }
+
+    Context 'When the RoleAssignment does not exist at all (no role assignment name, resource group, or principal ID match)' {
+        It 'returns $false' {
+            # Act
+            $result = Confirm-AzRoleAssignment -RoleAssignmentName 'role-assignment-does-not-exist' `
+                -ResourceGroupName $resourceGroupName -PrincipalDisplayName $principalDisplayName
+
+            # Assert
+            $result | Should -BeFalse
+        }
+    }
+
+    Context 'When the RoleAssignment rule exists for the specified PrincipalId in the correct resource group... ' {
+        It 'returns $true if no other parameters are provided' {
+            # Act
+            $result = Confirm-AzRoleAssignment -RoleAssignmentName $roleAssignmentName `
+                -ResourceGroupName $resourceGroupName -PrincipalDisplayName $principalDisplayName
+
+            # Assert
+            $result | Should -BeTrue
+        }
+
+        It 'returns $true if all parameters match' {
+            # Act
+            $result = Confirm-AzRoleAssignment -RoleAssignmentName $roleAssignmentName `
+                -ResourceGroupName $resourceGroupName -PrincipalDisplayName $principalDisplayName `
+                -RoleDefinitionName $roleDefinitionName -Scope $scope
+
+            # Assert
+            $result | Should -BeTrue
+        }
+
+        It 'returns $false if the RoleDefinition does not match' {
+            # Act
+            $result = Confirm-AzRoleAssignment -RoleAssignmentName $roleAssignmentName `
+                -ResourceGroupName $resourceGroupName -PrincipalDisplayName $principalDisplayName `
+                -RoleDefinitionName 'Reader' -Scope $scope
+
+            # Assert
+            $result | Should -BeFalse
+        }
+
+        It 'returns $false if the Scope does not match' {
+            # Act
+            $result = Confirm-AzRoleAssignment -RoleAssignmentName $roleAssignmentName `
+                -ResourceGroupName $resourceGroupName -PrincipalDisplayName $principalDisplayName `
+                -RoleDefinitionName $roleDefinitionName -Scope "/subscriptions/$subscriptionId"
+
+            # Assert
+            $result | Should -BeFalse
+        }
+    }
+
+    AfterAll {
+        Remove-AzResourceGroup -Name $resourceGroupName -Force
+        Remove-AzADServicePrincipal -ObjectId $principalId
+    }
+}
+
+Describe 'Confirm-AzRoleAssignment Unit Tests' -Tag 'Unit', 'Azure' {
+    BeforeAll {
+        function Import-Module {}
+        function Get-AzContext { return $true }
+    }
+    
+    Context 'When the role assignment does not exist at all (no role name, principal name, or resource group match)' {
+        It 'returns $false' {
+            # Arrange
+            function Get-AzRoleAssignment { return $null }
+
+            # Act
+            $result = Confirm-AzRoleAssignment -RoleAssignmentName 'role-assignment-does-not-exist' -PrincipalDisplayName $principalDisplayName `
+            -RoleDefinitionName $roleDefinitionName -Scope $scope -ResourceGroupName $resourceGroupName
+
+            # Assert
+            $result | Should -BeFalse
+        }
+    }
+
+    Context 'When the role assignment exists in the correct scope (and resource group)... ' {
+        BeforeAll {
+            # Arrange - Create a mock role assignment to test against
+            $mockRoleAssignment = [PSCustomObject]@{
+                RoleAssignmentName = $roleAssignmentName
+                DisplayName = $principalDisplayName
+                RoleDefinitionName = $roleDefinitionName
+                Scope = $scope
+                ResourceGroupName = $resourceGroupName
+            }
+
+            function Get-AzRoleAssignment { return $mockRoleAssignment }
+        }
+        It 'returns $true if no other parameters are provided' {
+            # Act
+            $result = Confirm-AzRoleAssignment -RoleAssignmentName $roleAssignmentName -PrincipalDisplayName $principalDisplayName `
+            -ResourceGroupName $resourceGroupName
+
+            # Assert
+            $result | Should -BeTrue
+        }
+
+        It 'returns $true if all parameters match' {
+            # Act
+            $result = Confirm-AzRoleAssignment -RoleAssignmentName $roleAssignmentName -PrincipalDisplayName $principalDisplayName `
+            -RoleDefinitionName $roleDefinitionName -Scope $scope -ResourceGroupName $resourceGroupName
+
+            # Assert
+            $result | Should -BeTrue
+        }
+
+        It 'returns $false if the role definition name does not match' {
+            # Act
+            $result = Confirm-AzRoleAssignment -RoleAssignmentName $roleAssignmentName -PrincipalDisplayName $principalDisplayName `
+            -RoleDefinitionName 'DifferentRoleDefinitionName' -Scope $scope -ResourceGroupName $resourceGroupName
+
+            # Assert
+            $result | Should -BeFalse
+        }
+
+        It 'returns $false if the scope does not match' {
+            # Act
+            $result = Confirm-AzRoleAssignment -RoleAssignmentName $roleAssignmentName -PrincipalDisplayName $principalDisplayName `
+            -RoleDefinitionName $roleDefinitionName -Scope 'DifferentScope' -ResourceGroupName $resourceGroupName
+
+            # Assert
+            $result | Should -BeFalse
+        }
+
+        It 'returns $false if the principal display name does not match' {
+            # Act
+            $result = Confirm-AzRoleAssignment -RoleAssignmentName $roleAssignmentName -PrincipalDisplayName 'DifferentPrincipalDisplayName' `
+            -RoleDefinitionName $roleDefinitionName -Scope $scope -ResourceGroupName $resourceGroupName
+
+            # Assert
+            $result | Should -BeFalse
+        }
+    }
+}
