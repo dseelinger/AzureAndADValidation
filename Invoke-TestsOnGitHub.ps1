d327b9836491ad7a568c34cc716b19b833113dbf
Doug Seelinger <doseelin@microsoft.com>
2024-09-06 12:49:32 -0400

    debugging

--- Invoke-TestsOnGitHub.ps1
+++ Invoke-TestsOnGitHub.ps1
@@ -5,25 +5,8 @@ Import-Module .\AzureAndADValidation.psd1
 # Create a Pester configuration object
 $config = [PesterConfiguration]::Default
 $config.CodeCoverage.Enabled = $true
-$config.Filter.ExcludeTag = @('AD-Integration', 'Skip-on-GitHub')
+# $config.Filter.ExcludeTag = @('AD-Integration', 'Skip-on-GitHub')
+$config.Filter.ExcludeTag = @('Skip-on-GitHub')
 
 # Run Pester with the configuration object and collect code coverage
-$testResults = Invoke-Pester -Configuration $config
-
-# Get the code coverage percentage
-# $codeCoverage = $testResults.CodeCoverage.CoveragePercent
-$testResults.CodeCoverage.CoveragePercent
-
-# # Check if any tests failed
-# if ($testResults.FailedCount -gt 0) {
-#     Write-Error "Some tests failed."
-#     exit 1
-# }
-
-# # Check if code coverage is below 75%
-# if ($codeCoverage -lt 75) {
-#     Write-Error "Code coverage is below 75%. Actual: $codeCoverage%"
-#     exit 1
-# }
-
-# Write-Output "All tests passed and code coverage is $codeCoverage%"
+Invoke-Pester -Configuration $config
